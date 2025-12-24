89b24a4413e5c12f6ef034ec7358bb21baf219bd
GabrielSouzaFullStack <gabrieldesouza@cesurg.com>
2025-12-24 18:45:18 -0300

    Corrige caminhos para uso relativo

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if ($sql->rowCount() > 0) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>To-Do-List</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="/src/style/style.css">
+  <link rel="stylesheet" href="src/style/style.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
@@ -23,7 +23,7 @@ if ($sql->rowCount() > 0) {
   <div id="todo">
     <h1>Todo List</h1>
 
-    <form action="/actions/create.php" method="POST" class="to-do-form">
+    <form action="actions/create.php" method="POST" class="to-do-form">
       <input type="text" name="description" placeholder="Digite sua tarefa aqui" required>
       <button type="submit" class="form-button">
         <i class="fa-solid fa-plus"></i>
@@ -35,8 +35,8 @@ if ($sql->rowCount() > 0) {
 
         <div class="task">
           <input type="checkbox" name="progress" class="progress <?= $task['tarefa_finalizada'] ? 'done' : '' ?>"
-          data-task-id="<?= $task['id'] ?>"
-          <?= $task['tarefa_finalizada'] ? 'checked' : '' ?>>
+            data-task-id="<?= $task['id'] ?>"
+            <?= $task['tarefa_finalizada'] ? 'checked' : '' ?>>
 
           <p class="task-description"><?= $task['descricao'] ?></p>
 
@@ -44,7 +44,7 @@ if ($sql->rowCount() > 0) {
             <a class="action-button edit-button">
               <i class="fa-regular fa-pen-to-square"></i>
             </a>
-            <a href="/actions/delete.php?id=<?= $task['id'] ?>" class="action-button delete-button">
+            <a href="actions/delete.php?id=<?= $task['id'] ?>" class="action-button delete-button">
               <i class="fa-solid fa-trash"></i>
             </a>
             <form action="actions/update.php" method="POST" class="to-do-form edit-task hidden">
@@ -61,7 +61,7 @@ if ($sql->rowCount() > 0) {
     </div>
   </div>
 
-  <script src="/src/javascript/script.js"></script>
+  <script src="src/javascript/script.js"></script>
 </body>
 
 </html>
